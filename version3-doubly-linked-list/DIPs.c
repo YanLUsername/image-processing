097c3219d8f31a1ce2e5b2f2702e2156659b376f
Yan Li <98135872+YanLUsername@users.noreply.github.com>
2025-04-04 00:54:22 -0700

    Update DIPs.c

--- version3-doubly-linked-list/DIPs.c
+++ version3-doubly-linked-list/DIPs.c
@@ -1,7 +1,10 @@
-/*********************************************************************/
-/* Homework Assignment 5, for EECS 22, Winter 2024                   */
-/* DIPs.c: source file for DIP operations                            */
-/*********************************************************************/
+/* MovieLab.c
+Assignment 5
+
+Date      Author      Notes
+3/23/25   Yan Li      Initial version
+4/4/25    Yan Li      Initial upload to GitHub
+*/
 
 #include <stdlib.h>
 #include <assert.h>
