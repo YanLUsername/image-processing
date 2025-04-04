a1ce10c85d2b3519f3f6a6a6a761ec477c2f950a
Yan Li <98135872+YanLUsername@users.noreply.github.com>
2025-04-04 00:50:20 -0700

    Update Advanced.c

--- version2-dynamic/Advanced.c
+++ version2-dynamic/Advanced.c
@@ -29,22 +29,22 @@ Image *Crop(Image *image, int x, int y, int W, int H) {
         Image *temp = CreateImage(W, H);
         assert(temp != NULL); // Ensure memory allocation succeeded
 
-	      // Copy pixels from the original image to the cropped image
-    	  for (int newY = 0; newY < H; newY++) {
-        	      for (int newX = 0; newX < W; newX++) {
-            		        SetPixelR(temp, newX, newY, GetPixelR(image, x + newX, y + newY));
-            		        SetPixelG(temp, newX, newY, GetPixelG(image, x + newX, y + newY));
-            		        SetPixelB(temp, newX, newY, GetPixelB(image, x + newX, y + newY));
-        	      }
-    	  }
+	// Copy pixels from the original image to the cropped image
+    	for (int newY = 0; newY < H; newY++) {
+        	for (int newX = 0; newX < W; newX++) {
+            		SetPixelR(temp, newX, newY, GetPixelR(image, x + newX, y + newY));
+            		SetPixelG(temp, newX, newY, GetPixelG(image, x + newX, y + newY));
+            		SetPixelB(temp, newX, newY, GetPixelB(image, x + newX, y + newY));
+        	}
+    	}
 
-	      DeleteImage(image);
- 	      return temp;
+	DeleteImage(image);
+ 	return temp;
 }
 
 /* Resize */
 Image *Resize(Image *image, int newWidth, int newHeight) {
-	      assert(image != NULL);     // Ensure the image pointer is valid
+	assert(image != NULL);     // Ensure the image pointer is valid
         assert(image->R != NULL);  // Ensure the R channel pointer is valid
         assert(image->G != NULL);  // Ensure the G channel pointer is valid
         assert(image->B != NULL);  // Ensure the B channel pointer is valid
@@ -61,16 +61,16 @@ Image *Resize(Image *image, int newWidth, int newHeight) {
         Image *temp = CreateImage(newWidth, newHeight);
         assert(temp != NULL); // Ensure memory allocation succeeded
 
-	      for (int y = 0; y < newHeight; y++) {
-	              for (int x = 0; x < newWidth; x++) {
-            		        int srcX = (int)(x * scaleX);
-            		        int srcY = (int)(y * scaleY);
+	for (int y = 0; y < newHeight; y++) {
+	        for (int x = 0; x < newWidth; x++) {
+            		int srcX = (int)(x * scaleX);
+            		int srcY = (int)(y * scaleY);
 
-            		        SetPixelR(temp, x, y, GetPixelR(image, srcX, srcY));
-            		        SetPixelG(temp, x, y, GetPixelG(image, srcX, srcY));
-            		        SetPixelB(temp, x, y, GetPixelB(image, srcX, srcY));
-        	      }
-    	  }
+            		SetPixelR(temp, x, y, GetPixelR(image, srcX, srcY));
+            		SetPixelG(temp, x, y, GetPixelG(image, srcX, srcY));
+            		SetPixelB(temp, x, y, GetPixelB(image, srcX, srcY));
+        	}
+    	}
 
         DeleteImage(image);
         return temp;
@@ -78,66 +78,65 @@ Image *Resize(Image *image, int newWidth, int newHeight) {
 
 /* Watermark */
 Image *Watermark(Image *image, const Image *watermark_image) {
-	      assert(image != NULL);
+	assert(image != NULL);
         assert(watermark_image != NULL);
         assert(image->R != NULL);
         assert(image->G != NULL);
         assert(image->B != NULL);
 
-	      for (int y = 0; y < ImageHeight(image); y++) {
-	              for (int x = 0; x < ImageWidth(image); x++) {
-            		        // Get watermark pixel (using modulo for tiling)
-            		        int wm_x = x % ImageWidth(watermark_image);
-            		        int wm_y = y % ImageHeight(watermark_image);
+	for (int y = 0; y < ImageHeight(image); y++) {
+	        for (int x = 0; x < ImageWidth(image); x++) {
+            		// Get watermark pixel (using modulo for tiling)
+            		int wm_x = x % ImageWidth(watermark_image);
+            		int wm_y = y % ImageHeight(watermark_image);
 
-            		        int wm_r = GetPixelR(watermark_image, wm_x, wm_y);
-            		        int wm_g = GetPixelG(watermark_image, wm_x, wm_y);
-            		        int wm_b = GetPixelB(watermark_image, wm_x, wm_y);
+            		int wm_r = GetPixelR(watermark_image, wm_x, wm_y);
+            		int wm_g = GetPixelG(watermark_image, wm_x, wm_y);
+            		int wm_b = GetPixelB(watermark_image, wm_x, wm_y);
 
-            		        // If watermark pixel is black (0,0,0), apply the effect
-            		        if (wm_r == 0 && wm_g == 0 && wm_b == 0) {
-                		            int new_r = GetPixelR(image, x, y) * 1.45;
-                		        int new_g = GetPixelG(image, x, y) * 1.45;
-                		        int new_b = GetPixelB(image, x, y) * 1.45;
+            		// If watermark pixel is black (0,0,0), apply the effect
+            		if (wm_r == 0 && wm_g == 0 && wm_b == 0) {
+                		int new_r = GetPixelR(image, x, y) * 1.45;
+                		int new_g = GetPixelG(image, x, y) * 1.45;
+                		int new_b = GetPixelB(image, x, y) * 1.45;
 
-                		        // Clamp values to avoid overflow
-                		        SetPixelR(image, x, y, new_r > 255 ? 255 : new_r);
-                		        SetPixelG(image, x, y, new_g > 255 ? 255 : new_g);
-                		        SetPixelB(image, x, y, new_b > 255 ? 255 : new_b);
-            		        }
-        	      }
-    	  }
-	      watermark_image = NULL;
+                		// Clamp values to avoid overflow
+                		SetPixelR(image, x, y, new_r > 255 ? 255 : new_r);
+                		SetPixelG(image, x, y, new_g > 255 ? 255 : new_g);
+                		SetPixelB(image, x, y, new_b > 255 ? 255 : new_b);
+            		}
+        	}
+    	}
+	watermark_image = NULL;
         return image;
 }
 
 /* Rotate by 90 */
 Image *RotateBy90(Image *image, int rotateDirection) {
-	      assert(image != NULL);
+	assert(image != NULL);
         assert(image->R != NULL);
         assert(image->G != NULL);
         assert(image->B != NULL);
         assert(rotateDirection == 0 || rotateDirection == 1);
 
-    	  Image *temp = CreateImage(ImageHeight(image), ImageWidth(image));
+    	Image *temp = CreateImage(ImageHeight(image), ImageWidth(image));
 
-    	  for (int y = 0; y < ImageHeight(image); y++) {
-        	      for (int x = 0; x < ImageWidth(image); x++) {
-            		        char r = GetPixelR(image, x, y);
-            		        char g = GetPixelG(image, x, y);
-            		        char b = GetPixelB(image, x, y);
-            		        if (rotateDirection == 0) { // Clockwise
-                		            SetPixelR(temp, ImageHeight(image) - 1 - y, x, r);
-                		            SetPixelG(temp, ImageHeight(image) - 1 - y, x, g);
-	                	            SetPixelB(temp, ImageHeight(image) - 1 - y, x, b);
-        	    	        } else { // Counterclockwise
-                		            SetPixelR(temp, y, ImageWidth(image) - 1 - x, r);
-                		            SetPixelG(temp, y, ImageWidth(image) - 1 - x, g);
-                		            SetPixelB(temp, y, ImageWidth(image) - 1 - x, b);
-            		        }
-        	      }
-   	    }
-
+    	for (int y = 0; y < ImageHeight(image); y++) {
+        	for (int x = 0; x < ImageWidth(image); x++) {
+            		char r = GetPixelR(image, x, y);
+            		char g = GetPixelG(image, x, y);
+            		char b = GetPixelB(image, x, y);
+            		if (rotateDirection == 0) { // Clockwise
+                		SetPixelR(temp, ImageHeight(image) - 1 - y, x, r);
+                		SetPixelG(temp, ImageHeight(image) - 1 - y, x, g);
+	                	SetPixelB(temp, ImageHeight(image) - 1 - y, x, b);
+        	    	} else { // Counterclockwise
+                		SetPixelR(temp, y, ImageWidth(image) - 1 - x, r);
+                		SetPixelG(temp, y, ImageWidth(image) - 1 - x, g);
+                		SetPixelB(temp, y, ImageWidth(image) - 1 - x, b);
+            		}
+        	}
+   	}
         DeleteImage(image);
         return temp;
 }
